a0f10f43236826fc6ef2ba0f01a1e328e703ab95
parisabrz <49725111+parisabrz@users.noreply.github.com>
2024-01-16 07:17:17 +0330

    add ngrx state management to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { provideStore } from "@ngrx/store";
+import { provideEffects } from "@ngrx/effects";
+import { BookReducer } from "./books/book.reducer";
+import { BookEffects } from "./books/book.effects";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideStore({ book: BookReducer }),
+    provideEffects([BookEffects]),
+    provideStoreDevtools(),
+  ],
 };
---
+++ src/app/books/book.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Observable, of, throwError } from "rxjs";
+import { Book } from "../models/book";
+
+@Injectable({
+  providedIn: "root",
+})
+export class BookService {
+  constructor() {}
+
+  addBook(book: Book): Observable<Book> {
+    return of(book);
+  }
+}
